0beb7235daf366fab0951d438b9ba5c615bb1b4f
nimarya <masha.nichiporenko@gmail.com>
2023-10-08 11:31:38 +0300

    added props to AppButton

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,13 +1,28 @@
 export default {
     template: `
-        <button class="bg-gray-200 hover:bg-gray-400 rounded px-4 py-2 font-bold m-4 disabled:cursor-not-allowed" :disabled="processing">
+        <button
+            :class="{
+                'rounded px-4 py-2 font-bold m-4 disabled:cursor-not-allowed': true,
+                'bg-gray-200 hover:bg-gray-400': type === 'primary',
+                'bg-purple-200 hover:bg-purple-400': type === 'secondary',
+            }"
+
+            :disabled="processing"
+        >
             <slot />
         </button>
     `,
 
+    props: {
+        type: {
+            type: String,
+            default: 'primary'
+        },
+    },
+
     data() {
         return {
-            processing: true,
+            processing: false,
         }
     }
-}
+};

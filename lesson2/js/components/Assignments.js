b590d77268814abef6196ebebb9e3ddbfe40ab57
nimarya <masha.nichiporenko@gmail.com>
2023-10-15 11:14:11 +0300

    made form work

--- lesson2/js/components/Assignments.js
+++ lesson2/js/components/Assignments.js
@@ -9,7 +9,7 @@ export default {
             <assignments-list :assignments="filters.completedAssignments" title="Completed tasks"></assignments-list>
 
             <form class="mt-6 ml-2" @submit.prevent="add">
-                <input class="placeholder:text-white bg-slate-500 text-white rounded-md p-2" type="text" placeholder="New assignment..">
+                <input v-model="newAssignment" class="placeholder:text-white bg-slate-500 text-white rounded-md p-2" type="text" placeholder="New assignment..">
                 <button class="ml-6 bg-slate-500 p-2 text-white rounded-md hover:bg-slate-400" type="submit">Add</button>
             </form>
         </div>
@@ -21,7 +21,8 @@ export default {
                 {name: 'first task very very very long', isCompleted: false, id: 1},
                 {name: 'second task', isCompleted: false, id: 2},
                 {name: 'third task', isCompleted: false, id: 3},
-            ]
+            ],
+            newAssignment: '',
         }
     },
 
@@ -36,7 +37,12 @@ export default {
 
     methods: {
         add() {
-            alert('hiii');
+            this.assignments.push({
+                name: this.newAssignment,
+                isCompleted: false,
+                id: this.assignments.length + 1,
+            });
+            this.newAssignment = '';
         }
     }
 }
